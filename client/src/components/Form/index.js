0e74f31431f0a6485b3be3c5734e2c87b1c6cb30
Natasa <mijatovicn650@gmail.com>
2022-05-30 09:12:00 +0200

    front end for home page is finished

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,9 +1,37 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 const Form = () => {
+  const user = true;
   return (
-    <Col xs={12} md={4}>
-      Form
+    <Col xs={12} md={4} className="form-wrap">
+      {user ? (
+        <form className="create-form">
+          <h3>Creating a post</h3>
+          <section className="form-section">
+            <label htmlFor="creator">Creator</label>
+            <input type="text" id="creator" name="creator" />
+          </section>
+          <section className="form-section">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" />
+          </section>
+          <section className="form-section">
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" />
+          </section>
+          <section className="form-section">
+            <label htmlFor="tags">Tags</label>
+            <textarea id="tags" name="tags" />
+          </section>
+          <section className="form-section">
+            <input type="file" className="" />
+          </section>
+          <button className="basic-btn button-5">Submit</button>
+          <button className="basic-btn button-5">Clear</button>
+        </form>
+      ) : (
+        <p className="create-form">Please sign in to create a post.</p>
+      )}
     </Col>
   );
 };
--- client/src/components/Posts/Post/index.js
+++ client/src/components/Posts/Post/index.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Card } from "react-bootstrap";
+import { BsHandThumbsUp } from "react-icons/bs";
+import icon from "../../../images/logo.png";
 
 const Post = () => {
+  const user = true;
   return (
     <Col xs={12} md={6}>
-      Post
+      <Card>
+        <Card.Img variant="top" src={icon} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle>Card Subtitle</Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <section>
+            {user && <button className="small-btn button-5">Edit</button>}
+            <p className="tags">tags</p>
+          </section>
+          <section>
+            {user ? (
+              <button className="transparent-btn hover">
+                <BsHandThumbsUp />
+                &nbsp; Like 6
+              </button>
+            ) : (
+              <button disabled className="transparent-btn crossed">
+                <BsHandThumbsUp />
+                &nbsp; Like 6
+              </button>
+            )}
+          </section>
+        </Card.Body>
+      </Card>
     </Col>
   );
 };
